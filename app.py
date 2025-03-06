e6bc43e7a6cefdc7228b07ed66dfb34fc7ef34f5
Moatez Tilouche <moateztilouch@gmail.com>
2025-03-06 14:12:05 +0100

    Added /status endpoint

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
-# app .py
-print ("Hello , CI/CD!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, CI/CD!"
+
+@app.route('/status')
+def status():
+    return {"status": "running", "message": "API is working"}
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
